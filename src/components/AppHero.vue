8f33b42ddc97107e5ec9d3eba8bf6688ae448424
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-19 16:51:46 +0100

    replaced static image by dynamic image

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -1,9 +1,19 @@
 <script>
 export default {
     name: 'AppHero',
+    data() {
+        return {
+            image: 'matrix.jpg',
+        }
+    },
+    methods: {
+        buildImgPath(img) {
+            return new URL(`../assets/img/${img}`, import.meta.url).href
+        }
+    }
 }
 </script>
 
 <template>
-    <img src="../assets/img/matrix.jpg" alt="matrix">
+    <img :src="buildImgPath(image)" alt="matrix">
 </template>
